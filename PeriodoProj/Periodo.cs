00223c25bf1defe59d0594e435554a369c065eba
rodrigoebravo <rbravo@cnp.com.ar>
2019-08-23 13:47:04 -0300

    Desarrollo de funcionalidades

--- PeriodoProj/Periodo.cs
+++ PeriodoProj/Periodo.cs
@@ -13,27 +13,10 @@ namespace PeriodoProj
         public DateTime? Date { get; private set; }
         public int Year { get { if (this.IsValid) return this.Date.Value.Year; return 0; } }
         public int Month { get { if (this.IsValid) return this.Date.Value.Month; return 0; } }
+        public static Periodo MinValue { get { return new Periodo(190001); } }
+        public static Periodo MaxValue { get { return new Periodo(DateTime.MaxValue); } }
+        public static Periodo Current { get { return new Periodo(DateTime.Now); } }
 
-        /// <summary>
-        /// Valida el periodo del parametro
-        /// </summary>
-        /// <returns></returns>
-        private static bool Validar(string valorPeriodo)
-        {
-            if (valorPeriodo.ToString().Length != 6)
-                return false;
-
-            var valorAnio = int.Parse(valorPeriodo.Substring(0, 4));
-            var valorMes = int.Parse(valorPeriodo.Substring(4, 2));
-
-            if (valorAnio < 1900)
-                return false;
-
-            if (valorMes < 1 || valorMes > 12)
-                return false;
-
-            return true;
-        }
         public Periodo(int periodo) : this(periodo.ToString()) { }
         public Periodo(string periodo)
         {
@@ -47,6 +30,32 @@ namespace PeriodoProj
             this.Value = null;
             this.Date = null;
             this.IsValid = false;
+        }
+        public Periodo(DateTime periodo) : this(TraerPeriodo(periodo))
+        {
+
+        }
+        private static bool Validar(string periodo)
+        {
+            if (periodo.ToString().Length != 6)
+                return false;
+
+            var valorAnio = int.Parse(periodo.Substring(0, 4));
+            var valorMes = int.Parse(periodo.Substring(4, 2));
+
+            if (valorAnio < 1900)
+                return false;
+
+            if (valorMes < 1 || valorMes > 12)
+                return false;
+
+            return true;
+        }
+        private static string TraerPeriodo(DateTime periodo)
+        {
+            if (periodo != null)
+                return $"{periodo.Year.ToString().PadLeft(4, '0')}{periodo.Month.ToString().PadLeft(2, '0')}";
+            return string.Empty;
         }
 
         public static explicit operator Periodo(DateTime fecha)
@@ -74,7 +83,7 @@ namespace PeriodoProj
                 mes += 12;
             }
             var resAnio = anio.ToString();
-            var resMes = mes < 10 ? mes.ToString().PadLeft(2, '0') : mes.ToString();
+            var resMes = mes.ToString().PadLeft(2, '0');
             return new Periodo($"{resAnio}{resMes}");
         }
         public static Periodo operator -(Periodo periodo, int meses)
@@ -96,11 +105,53 @@ namespace PeriodoProj
         {
             return !(periodoA == periodoB);
         }
+        public static bool operator >(Periodo periodoA, Periodo periodoB)
+        {
+            return periodoA.Date > periodoB.Date;
+        }
+        public static bool operator <(Periodo periodoA, Periodo periodoB)
+        {
+            return periodoA.Date < periodoB.Date;
+        }
+        public static bool operator >=(Periodo periodoA, Periodo periodoB)
+        {
+            return periodoA.Date >= periodoB.Date;
+        }
+        public static bool operator <=(Periodo periodoA, Periodo periodoB)
+        {
+            return periodoA.Date <= periodoB.Date;
+        }
         public override string ToString()
         {
             if (this.IsValid)
                 return this.Value.ToString();
             return string.Empty;
         }
+        public string ToString(string format)
+        {
+            if (!this.IsValid)
+                return string.Empty;
+            int cont = 0;
+
+            if (!format.Contains('.') || !format.Contains('-') || !format.Contains('/'))
+                return this.Value.ToString();
+
+            for (int i = 0; i < format.Length; i++)
+            {
+
+                if (format[i].Equals('#'))
+                    cont++;
+                else if (format[i].Equals('.') || format[i].Equals('-') || format[i].Equals('/'))
+                    break;
+            }
+            if (cont == 2)
+                return $"{this.Value.ToString().Substring(4, 2)}.{this.Value.ToString().Substring(0, 4)}";
+
+            if (cont == 4)
+                return $"{this.Value.ToString().Substring(0, 4)}.{this.Value.ToString().Substring(4, 2)}";
+
+            return this.Value.ToString();
+        }
+
     }
 }
